cfc7f682e89a70ad2b698bfe892020f4b29bdb57
Maurizio Abbà <maurizio.abba00@gmail.com>
2013-01-04 12:28:23 +0100

    userReview 2.1 travelReview 2.1
    Aggiunti pulsanti di navigazione tra le pagine: unica cosa mancante è
    il carosel a lato!

--- app/scripts/user_review.js
+++ app/scripts/user_review.js
@@ -1,5 +1,8 @@
 define(['fill_user_info', 'mapTest', 'set_travel_list', 'database'], function (UserInfo, mapTest, travelList) {
 	var num = 3;
+	var match = window.location.href.match(/\?user=(\d)/);
+	if (match)
+		num=match[1];
 	fillUserInfo(num);
 	setStaticMap(num);
 	setTravelList(num);
