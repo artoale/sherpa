f5724db16693cf2bd9f4f3046abe1fb15709fe2e
Alessandro Artoni <artoale@gmail.com>
2013-01-04 22:46:23 +0100

    Modifiche alla index  per NaN :)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,17 +13,17 @@ define(['trip'], function (trip) {
         $('#start').datepicker(options).on('changeDate', function (ev) {
             trip.startDate = ev.date;
             $(this).datepicker('setValue', trip.startDate);
-        }).datepicker('setValue', trip.startDate);
+        });
 
         $('#end').datepicker(options).on('changeDate', function (ev) {
             trip.endDate = ev.date;
             $(this).datepicker('setValue', trip.endDate);
-        }).datepicker('setValue', trip.endDate);
+        });
 
         $('#startPlanning').on('click', function () {
             trip.destination = $('input[name="destination"]').val();
             trip.save();
-            window.location.href = 'mysherpa.html';
+            window.location.href = 'specification.html';
         });
     });
 
